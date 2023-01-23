5f8d2614046bf9fc22cea564b3ad79d3d7f6dccb
Nicolas Lima <nicolaslima1500@gmail.com>
2023-01-23 18:40:26 -0300

    Para fixar 01

---
+++ fundamentos/secao-03-introudcao-a-java-script/dia-01-js-primeiros-passos/para-fixar/para-fixar-01/script.js
@@ -0,0 +1,18 @@
+const myName ='Nicolas';
+const birthCity = 'São Paulo';
+let birthYear = 2000;
+
+ console.log(myName);
+ console.log(birthCity);
+ console.log(birthYear);
+
+ birthYear = 2030;
+
+ console.log(birthYear)
+
+ birthCity = 2030;
+
+ console.log(birthCity);
+
+
+ 
