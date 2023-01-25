9097b96ecb62ac8702435dba44128d8f7180f82e
Nicolas Lima <nicolaslima1500@gmail.com>
2023-01-25 19:53:15 -0300

    Tópico 02 do exercício de hoje concluído

--- fundamentos/secao-03-introudcao-a-java-script/dia-03-logica-de-programacao-e-algoritmos/exercicios/exercicio-01/script.js
+++ fundamentos/secao-03-introudcao-a-java-script/dia-03-logica-de-programacao-e-algoritmos/exercicios/exercicio-01/script.js
@@ -1,3 +1,5 @@
+// TÓPICO 1
+
 // 1 - criar uma variável para poder receber um número
 // 2 - criar um laço de repetição para fazer as multiplicações necessárias
 // 3 - criar uma variável para armazenar as multiplicações 
@@ -5,25 +7,26 @@
 
 let numero = 10;
 let mult = 1;
-let resultado = 0;
 
 for(let index = numero; index >= 2; index -= 1){
     mult = mult * index;
-    if(index === 2){
-        console.log(mult);
-    }
 }
+    console.log(mult);
 
+// --------------------------------------------------------------------------------------------------------
 
+// TÓPICO 2
 
-// testando como exibir somente uma iteração específica
-// let exemplo = [1, 2, 3, 4, 5];
-// let ex = 0;
+// 1 - criar uma variável para receber a palavra invertida
+// 2 - percorrer a palavra contida na variável word
+// 3 - inverter a ordem das letras da plavra contida na variável word
+// 4 - imprimir a palavra invertida
 
-// for(let index = 0; index < exemplo.length; index += 1){
-//     ex = ex + exemplo[index];
-//     if(index === (exemplo.length - 1)){
-//         console.log(ex);
-//     }
-//     // console.log(ex);
-// }
+let word = 'Nicolas';
+
+let palavraInvertida = '';
+
+for(let index = word.length - 1; index >= 0; index -= 1) {
+    palavraInvertida = palavraInvertida + word[index];
+}
+    console.log(palavraInvertida);
