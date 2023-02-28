dbdc073421103d1a63e95378d81ee4702f2fbc25
Nicolas Lima <nicolaslima1500@gmail.com>
2023-02-28 11:44:09 -0300

    Exercício finalizado

--- fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
+++ fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
@@ -26,9 +26,9 @@ describe('a função createItem', () => {
     expect(() => {createItem(10, 'kg', 1.99, 20)}).toThrow('O nome do item deve ser uma string');
   });
 
-//   it('Lança um erro se o preço é negativo', () => {
-//     expect(() => {createItem('banana', 'kg', -1.99)}).toThrow('O preço do item deve ser maior que zero');
-//   });
+  it('Lança um erro se o preço é negativo', () => {
+    expect(() => {createItem('banana', 'kg', -1.99)}).toThrow('O preço do item deve ser maior que zero');
+  });
 
   it('Lança um erro se o preço é zero', () => {
     expect(() => {createItem('banana', 'kg', 0)}).toThrow('O preço do item deve ser maior que zero');
