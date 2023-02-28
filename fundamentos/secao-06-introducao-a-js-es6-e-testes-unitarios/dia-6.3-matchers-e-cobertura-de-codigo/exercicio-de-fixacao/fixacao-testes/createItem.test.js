b3b68718505adc4817ea79bc63c48c94d9374abe
Nicolas Lima <nicolaslima1500@gmail.com>
2023-02-28 10:03:57 -0300

    Primeiro teste finalizado

--- fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
+++ fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
@@ -1,9 +1,17 @@
 const createItem = require('./createItem');
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
+  it('cria um item válido', () => {
+    expect(createItem('banana', 'kg', 1.99, 20)).toMatchObject({
+        name: 'banana',
+        unit: 'kg',
+        price: 1.99,
+        quantity: 20,
+    });
+  });
+
+//   it.todo('utiliza zero como quantidade padrão');
+//   it.todo('Lança um erro quando não recebe parâmetros');
+//   it.todo('Lança um erro se o nome do item não é uma string');
+//   it.todo('Lança um erro se o preço é negativo');
+//   it.todo('Lança um erro se o preço é zero');
 });
