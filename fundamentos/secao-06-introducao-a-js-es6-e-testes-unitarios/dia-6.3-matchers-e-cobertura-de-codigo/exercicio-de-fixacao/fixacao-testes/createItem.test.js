a867f383f26aef9255a9153d79e286feaee1dd80
Nicolas Lima <nicolaslima1500@gmail.com>
2023-02-28 11:32:53 -0300

    Teste 03 finalizado

--- fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
+++ fundamentos/secao-06-introducao-a-js-es6-e-testes-unitarios/dia-6.3-matchers-e-cobertura-de-codigo/exercicio-de-fixacao/fixacao-testes/createItem.test.js
@@ -18,7 +18,10 @@ describe('a função createItem', () => {
     });
   });
 
-//   it.todo('Lança um erro quando não recebe parâmetros');
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+
 //   it.todo('Lança um erro se o nome do item não é uma string');
 //   it.todo('Lança um erro se o preço é negativo');
 //   it.todo('Lança um erro se o preço é zero');
